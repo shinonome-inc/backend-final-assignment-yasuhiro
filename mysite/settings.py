1b8929078480df74136dc303d4d54d0339d11a02
Yasushi400 <chen20030223@outlook.jp>
2022-12-15 13:27:56 +0900

    add static/base.css and fix mysetting/urls.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,7 +132,14 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-STATIC_URL = "/static/"
+# How to set static files
+# https://docs.djangoproject.com/ja/4.1/howto/static-files/
+
+STATIC_URL = "static/"
+
+STATICFILES_DIRS = {
+    BASE_DIR / "static",
+}
 
 AUTH_USER_MODEL = "accounts.User"
 
