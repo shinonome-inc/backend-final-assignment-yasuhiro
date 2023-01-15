c9814a37652af1a6c7f1d0c55de094c9ea6187da
Yasushi400 <chen20030223@outlook.jp>
2023-01-15 19:45:13 +0900

    fix

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth import authenticate, get_user_model, login, views
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, TemplateView
+
+from mysite.settings import LOGIN_REDIRECT_URL
 
 from .forms import LoginForm, SignUpForm
 
@@ -12,18 +14,15 @@ User = get_user_model()
 class SignUpView(CreateView):
     form_class = SignUpForm
     template_name = "accounts/signup.html"
-    success_url = reverse_lazy("tweets:home")
+    success_url = reverse_lazy(LOGIN_REDIRECT_URL)
 
     def form_valid(self, form):
         response = super().form_valid(form)
         username = form.cleaned_data.get("username")
         password = form.cleaned_data.get("password1")
         user = authenticate(self.request, username=username, password=password)
-        if user is not None:
-            login(self.request, user)
-            return response
-        else:
-            return redirect("welcome:top")
+        login(self.request, user)
+        return response
 
 
 class LoginView(views.LoginView):
@@ -36,7 +35,11 @@ class LogoutView(views.LogoutView):
 
 
 class UserProfileView(LoginRequiredMixin, TemplateView):
+    moder = User
+    context_object_name = "profile"
     template_name = "accounts/profile.html"
+    slug_field = "username"
+    slug_url_kwarg = "username"
 
-    def get_queryset(self):
-        return User.objects.get(id=self.request.user.id)
+    def get_context_data(self, **kwargs):
+        pass
--- mysite/urls.py
+++ mysite/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("tweets/", include("tweets.urls")),
-    path("/", include("welcome.urls")),
+    path("", include("welcome.urls")),
 ]
