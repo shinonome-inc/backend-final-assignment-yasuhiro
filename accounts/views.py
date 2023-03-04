af1f40461fd07f21fca3ab05c2188c8fb43d2af1
Yasushi400 <chen20030223@outlook.jp>
2023-03-05 02:40:25 +0900

    flack8

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.contrib.auth import authenticate, login, views
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView
 
