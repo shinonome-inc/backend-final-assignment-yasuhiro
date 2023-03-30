0feb3d66791d0de97ad45f17a0e0e42a96331f35
Yasushi400 <chen20030223@outlook.jp>
2023-03-30 14:48:40 +0900

    review change 2.2

--- tweets/views.py
+++ tweets/views.py
@@ -16,9 +16,7 @@ class HomeView(LoginRequiredMixin, ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["liked_tweets"] = (
-            Like.objects.select_related("user").filter(user=self.request.user).values_list("tweet", flat=True)
-        )
+        context["liked_tweets"] = Like.objects.filter(user=self.request.user).values_list("tweet", flat=True)
         return context
 
 
